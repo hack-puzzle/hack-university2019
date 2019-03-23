df6df056b7beb15ef96f023df426230432ce039c
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-03-23 17:41:37 +0300

    Add artist info on tab1

--- mts-concert/src/app/tab1/tab1.module.ts
+++ mts-concert/src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import {ArtistInfoComponent} from "../components/artist-info/artist-info.component";
 
 @NgModule({
   imports: [
@@ -12,6 +13,9 @@ import { Tab1Page } from './tab1.page';
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab1Page]
+  declarations: [
+      Tab1Page,
+      ArtistInfoComponent
+  ]
 })
 export class Tab1PageModule {}
