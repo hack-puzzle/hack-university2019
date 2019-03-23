151327c5d9d034731b50b9119de53ba9176fd878
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-03-23 18:54:52 +0300

    Add rest service

--- mts-concert/src/app/rest.service.ts
+++ mts-concert/src/app/rest.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Observable, timer} from "rxjs";
 
 	const httpOptions = {
 		headers: new HttpHeaders({'Content-Type': 'application/json'})
 	};
 	
-	const apiUrl = 'http://hack-university-server.herokuapp.com/';
+	const apiUrl = 'http://hack-university-server.herokuapp.com/' + "radiotapok/";
 
 @Injectable({
 	providedIn: 'root'
@@ -18,7 +19,7 @@ export class RestService {
 	getEvent(id) {
 		return new Promise((resolve, reject) => {
 			// let headers = new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem('token'));
-			this.http.get(apiUrl+"concert-start")
+			this.http.get(apiUrl + "concert-start")
 				.subscribe(res => {					
 					console.log(res); // log
 					resolve(res);
@@ -27,6 +28,30 @@ export class RestService {
 					reject(err);
 				});
 		});
+	}
+
+	getRequest(url: string) {
+		return new Promise((resolve, reject) => {
+			// let headers = new HttpHeaders().set("Authorization", "Bearer " + localStorage.getItem('token'));
+			this.http.get(apiUrl + "concert-start")
+				.subscribe(res => {
+					console.log(res); // log
+					resolve(res);
+				}, (err) => {
+					console.log(err); // log
+					reject(err);
+				});
+		});
+	}
+
+	longPolling() {
+		this.getRequest(apiUrl + "concert-update")
+			.then(res => {
+
+			})
+			.finally(() => {
+				timer(5000).subscribe(() => this.longPolling());
+			});
 	}
 	
 }
