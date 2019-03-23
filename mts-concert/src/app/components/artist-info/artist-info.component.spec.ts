b237abb4a6070a9966fefa8a7d1dabbb98c88635
Nikita Chernyakov <nikchernyakov@yandex.ru>
2019-03-23 17:32:59 +0300

    Add artist info on tab1

---
+++ mts-concert/src/app/components/artist-info/artist-info.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArtistInfoComponent } from './artist-info.component';
+
+describe('ArtistInfoComponent', () => {
+  let component: ArtistInfoComponent;
+  let fixture: ComponentFixture<ArtistInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ArtistInfoComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArtistInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
